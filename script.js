0fed3f32d445fc607292f66a6cb067082ea0eea0
Alex <algaraschukicloud.com@Alex.local>
2019-03-14 00:55:57 +0200

    media queris

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-$( document ).ready(function() {
-    $("#btn").click(
-		function(){
-			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+$(document).on('submit', '#ajax_form', function(){
+    sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
 			return false; 
-		}
-	);
-});
+})
 
 
 function sendAjaxForm(result_form, ajax_form, url) {
     $.ajax({
-        url:     'https://test-telega.piastrix24.com/api/get_data', //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
+        url:     'https://test-telega.piastrix24.com/api/get_data',
+        type:     "POST", 
+        dataType: "html", 
+        data: $("#"+ajax_form).serialize(),  
+        success: function(response) { 
         	let result = $.parseJSON(response);
             $('#result_form').html(
-            '<form class="pay-ready" action="https://test-telega.piastrix24.com/api/get_data"> '+
+            '<form class="pay-ready" action="https://pay.piastrix.com/ru/pay"> '+
                 'Amount: <input type="number" value="'+result.amount+'" name="amount">'+
                 'Currency: <input type="text" value="'+result.currency+'">'+
                 'Description: <input type="text" value="'+result.description+'">'+
@@ -27,18 +23,10 @@ function sendAjaxForm(result_form, ajax_form, url) {
                 '<input type="hidden" vlue="'+result.sign+'">'+
                 '<button class="m-btn" onclick="func();" href="#">Отправить</button>'+
             '</form>'
-            );
-
-            
+            ); 
         },
-       
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
      });
 }
-
-
 
 
 function func() {
