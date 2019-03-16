e0d14b5abef9816119f78f8ad5a4ac7ef0179636
Alex <algaraschukicloud.com@Alex.local>
2019-03-16 23:28:56 +0200

    modified ajax code

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function sendAjaxForm(result_form, ajax_form, url) {
         success: function(response) { 
         	let result = $.parseJSON(response);
             $('#result_form').html(
-            '<form class="pay-ready" action="https://pay.piastrix.com/ru/pay"> '+
+            '<form class="pay-ready" action="https://pay.piastrix.com/ru/pay" method="post"> '+
                 'Amount: <input type="number" value="'+result.amount+'" name="amount">'+
-                'Currency: <input type="text" value="'+result.currency+'">'+
-                'Description: <input type="text" value="'+result.description+'">'+
-                'Payway: <input type="text" value="'+result.payway+'">'+
-                '<input type="hidden" value="'+result.shop_id+'">'+
-                '<input type="hidden" value="'+result.shop_order_id+'">'+
-                '<input type="hidden" vlue="'+result.sign+'">'+
+                'Currency: <input type="text" value="'+result.currency+'" name="currency">'+
+                'Description: <input type="text" value="'+result.description+'" name="description">'+
+                'Payway: <input type="text" value="'+result.payway+'" name="payway">'+
+                '<input type="hidden" value="'+result.shop_id+'" name="shop_id">'+
+                '<input type="hidden" value="'+result.shop_order_id+'" name="shop_order_id">'+
+                '<input type="hidden" value="'+result.sign+'" name="sign">'+
                 '<button class="m-btn" onclick="func();" href="#">Отправить</button>'+
             '</form>'
             ); 
@@ -30,7 +30,7 @@ function sendAjaxForm(result_form, ajax_form, url) {
 
 
 function func() {
-    document.location.href = 'https://pay.piastrix.com/ru/pay'
+    // document.location.href = 'https://pay.piastrix.com/ru/pay'
   }
 
   
